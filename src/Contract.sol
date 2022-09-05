598cd47b5287537a38fea57e8e0763289ea37866
Chloe Tan <thryec@gmail.com>
2022-09-05 15:27:18 +0800

    v1 branch

--- src/Contract.sol
+++ src/Contract.sol
@@ -9,22 +9,21 @@ contract Wiki {
     Counters.Counter public articleId;
     Counters.Counter public versionId;
 
-    //article struct
-    struct article {
+    // article struct
+    struct Article {
         uint256 articleId
         uint256 versionId
-        //address lastEditor 
+        address articleCreator 
+        // address lastEditor 
     }
     
     // articles/hashes in universe
     mapping (string ipfsHash => boolean) public articles;
-
     mapping (uint256 articleId => mapping(uint256 versionId => string ipfsHash)) public versionTree;
 
     //map user actions to wallet addresses - who did what
     mapping (uint256 articleId => address user) public authors;
     mapping (uint256 versionId => address user) public editors;
-
 
     event Created( address indexed from, uint256 indexed articleId, uint256 indexed versionId, string ipfsHash);
     event Updated( address indexed from, uint256 indexed articleId, uint256 indexed versionId, string ipfsHash);
@@ -34,7 +33,7 @@ contract Wiki {
         
         require(articles(ipfsHash) == false);         // article hash should not exist
         
-        uint256 newArticleId = articleId.current();   // starts frm 0
+        uint256 newArticleId = articleId.current();   // starts from 0
         uint256 newVersionId = versionId.current();
 
         versionTree(newArticleId)() = 
